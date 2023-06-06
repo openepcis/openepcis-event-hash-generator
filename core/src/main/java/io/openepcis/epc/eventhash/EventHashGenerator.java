436daf4c45fb3ac73a64f54978f7ea24ef225e14
Aravinda Baliga B <aravinda.baliga@benelog.com>
2023-06-06 17:07:18 +0200

    ignore user-specific fields from pre-hash if provided and enabling cors to fix cors error.

--- core/src/main/java/io/openepcis/epc/eventhash/EventHashGenerator.java
+++ core/src/main/java/io/openepcis/epc/eventhash/EventHashGenerator.java
@@ -18,16 +18,14 @@ package io.openepcis.epc.eventhash;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.openepcis.constants.EPCIS;
+import io.openepcis.epc.eventhash.constant.ConstantEventHashInfo;
 import io.openepcis.epc.eventhash.exception.EventHashException;
 import io.openepcis.reactive.publisher.ObjectNodePublisher;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.subscription.MultiEmitter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
 import javax.xml.parsers.SAXParserFactory;
 import lombok.NoArgsConstructor;
@@ -37,7 +35,6 @@ import org.reactivestreams.Publisher;
 @Slf4j
 @NoArgsConstructor
 public class EventHashGenerator {
-
   private static final SAXParserFactory SAX_PARSER_FACTORY = SAXParserFactory.newInstance();
   private String prehashJoin = "";
 
@@ -51,6 +48,23 @@ public class EventHashGenerator {
 
   public void prehashJoin(final String s) {
     prehashJoin = s.replace("\\n", "\n").replace("\\r", "\r");
+  }
+
+  /**
+   * Method used to populate custom fields that needs to be ignored during the pre-hash generation
+   *
+   * @param ignoreFieldsForHash List of string element with field name which will be ignored during
+   *     the pre-hash generation.
+   */
+  public void customFieldsToIgnore(final String ignoreFieldsForHash) {
+    final List<String> ignoreFieldsList =
+        Arrays.stream(ignoreFieldsForHash.split(",")).map(String::trim).toList();
+
+    // Clear the existing element if any
+    ConstantEventHashInfo.clearFieldsToExclude();
+
+    // Add the provided elements to List which will be ignored during pre-hash generation
+    ConstantEventHashInfo.addFieldsToExclude(ignoreFieldsList);
   }
 
   /**
